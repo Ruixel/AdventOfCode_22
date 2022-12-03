4a2e8bc93f6e1e614e201ea61fbbd3d7c575f448
Ruixel <13946497+Ruixel@users.noreply.github.com>
2022-12-03 09:21:32 +0000

    nice linq solution for day 3

--- AoC22/day3.cs
+++ AoC22/day3.cs
@@ -7,42 +7,28 @@ public class Day3
 		var input = System.IO.File.ReadAllText("..\\..\\..\\day3.txt");
 
 		var rucksacks = input.Split("\r\n");
-		var part1 = 0;
-		foreach (var rucksack in rucksacks)
-		{
-			var halfLength = rucksack.Length / 2;
-			var r1 = rucksack.Take(halfLength).ToHashSet();
-			var r2 = rucksack.Skip(halfLength).ToHashSet();
 
-			r1.IntersectWith(r2);
-			var duplicate = r1.First();
-			if (r1.Count > 1)
-				Console.WriteLine($"more than one duplicate {rucksack}");
-
-			if (Char.IsLower(duplicate))
-				part1 += duplicate - (int) 'a' + 1;
-			else
-				part1 += duplicate - (int) 'A' + 27;
-		}
-
-		var part2 = 0;
-		for (int i = 0; i < rucksacks.Length; i += 3)
-		{
-			var r1 = rucksacks[i].ToHashSet();
-			var r2 = rucksacks[i + 1].ToHashSet();
-			var r3 = rucksacks[i + 2].ToHashSet();
-
-			r1.IntersectWith(r2);
-			r1.IntersectWith(r3);
-			var badge = r1.First();
-
-			if (Char.IsLower(badge))
-				part2 += badge - (int) 'a' + 1;
-			else
-				part2 += badge - (int) 'A' + 27;
-		}
+		var part1 = rucksacks
+			.Select(r => r
+				.Chunk(r.Length / 2)
+				.Select(n => n.ToHashSet())
+				.Aggregate((st, r) => st.Intersect(r).ToHashSet())
+				.First())
+			.Select(ch => Char.IsLower(ch) ? ch - 'a' + 1 : ch - 'A' + 27)
+			.Sum();
 
 		Console.WriteLine($"Part1: {part1}");
+
+		var part2 = rucksacks
+			.Chunk(3)
+			.Select(c => c
+				.Select(set => set.ToHashSet())
+				.Aggregate((st, r) => st.Intersect(r).ToHashSet())
+				.First()
+			)
+			.Select(ch => Char.IsLower(ch) ? ch - 'a' + 1 : ch - 'A' + 27)
+			.Sum();
+
 		Console.WriteLine($"Part2: {part2}");
 	}
 }
