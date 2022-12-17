04f82de8364f3ff35f24ab02d4dd5128b49938e8
Ruixel <13946497+Ruixel@users.noreply.github.com>
2022-12-17 17:07:16 +0000

    Day 14 part 1

--- AoC22/Program.cs
+++ AoC22/Program.cs
@@ -1,137 +1,164 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Runtime.InteropServices.ComTypes;
 
-var rx = new Regex(@"(\d+),(\d+)");
-var lines = File.ReadLines("../../../day14.txt")
-    .Select(line =>
-    {
-        var matches = rx.Match(line);
-        var coords = new List<Pos>();
-        foreach (Match match in rx.Matches(line))
-        {
-            var split = match.Value.Split(",");
-            coords.Add(new Pos(int.Parse(split[0]), int.Parse(split[1])));
-        }
+var wind = File.ReadAllText("../../../day15.txt").ToCharArray();
 
-        return coords;
-    })
-    .ToList();
-
-var grid = new char[1000, 1000];
-for (var y = 0; y < grid.GetLength(0); y++)
+// L = Left Collision
+// R = Right Collision
+//
+// X = Left/Down Collision
+// Y = Right/Down Collision
+var rockA = new char[,]
 {
-    for (var x = 0; x < grid.GetLength(1); x++)
-    {
-        grid[y, x] = '.';
-    }
+	{'@', '@', '@', '@'},
+	{'.', '.', '.', '.'},
+	{'.', '.', '.', '.'},
+	{'.', '.', '.', '.'}
+};
+
+var rockB = new char[,]
+{
+	{'.', '@', '.', '.'},
+	{'@', '@', '@', '.'},
+	{'.', '@', '.', '.'},
+	{'.', '.', '.', '.'}
+};
+
+var rockC = new char[,]
+{
+	{'@', '@', '@', '.'},
+	{'.', '.', '@', '.'},
+	{'.', '.', '@', '.'},
+	{'.', '.', '.', '.'}
+};
+
+var rockD = new char[,]
+{
+	{'@', '.', '.', '.'},
+	{'@', '.', '.', '.'},
+	{'@', '.', '.', '.'},
+	{'@', '.', '.', '.'}
+};
+
+var rockE = new char[,]
+{
+	{'@', '@', '.', '.'},
+	{'@', '@', '.', '.'},
+	{'.', '.', '.', '.'},
+	{'.', '.', '.', '.'}
+};
+
+var rocks = new[] {rockA, rockB, rockC, rockD, rockE};
+var rockHeights = new[] {1, 3, 3, 4, 2};
+
+var cave = new char[5000, 7];
+for (int y = 0; y < cave.GetLength(0); y++)
+{
+	for (int x = 0; x < cave.GetLength(1); x++)
+	{
+		cave[y, x] = '.';
+	}
 }
 
-void drawGrid(int x, int y, int w, int h)
+void RockLoop(char[,] rock, int x, int y, Action<int, int> func)
 {
-    for (int gy = y; gy < y + h; gy++)
-    {
-        for (int gx = x; gx < x + w; gx++)
-        {
-            Console.Write(grid![gy, gx]);
-        }
+	for (int rx = 0; rx < 4; rx++)
+	{
+		for (int ry = 0; ry < 4; ry++)
+		{
+			if (rock[ry, rx] == '@')
+			{
+				var caveX = rx + x;
+				var caveY = ry + y;
 
-        Console.WriteLine();
-    }
-
-    Console.WriteLine();
+				func(caveX, caveY);
+			}
+		}
+	}
 }
 
-var diff = 0;
-foreach (var lineChain in lines)
+var topY = 3;
+var windIndex = 0;
+for (int i = 0; i < 2022; i++)
 {
-    var x = lineChain[0].X;
-    var y = lineChain[0].Y;
-    for (int i = 1; i < lineChain.Count; i++)
-    {
-        var dx = lineChain[i].X - x;
-        var dy = lineChain[i].Y - y;
-        if (dx < 0) x = lineChain[i].X;
-        if (dy < 0) y = lineChain[i].Y;
+	var rock = rocks[i % 5];
+	var y = topY;
+	var x = 2;
+	
+	//Console.WriteLine("Spawned");
+	//DrawCave(0, 10, rock, x, y);
 
-        for (int lx = x; lx <= x + Math.Abs(dx); lx++)
-        {
-            for (int ly = y; ly <= y + Math.Abs(dy); ly++)
-            {
-                grid[ly, lx] = '#';
-            }
-        }
+	var landed = false;
+	while (!landed)
+	{
+		if (wind[windIndex] == '>')
+		{
+			var canMoveRight = true;
+			RockLoop(rock, x, y, ((caveX, caveY) =>
+			{
+				if (caveX + 1 >= 7 || cave[caveY, caveX + 1] == '#') canMoveRight = false;
+			}));
 
-        x = lineChain[i].X;
-        y = lineChain[i].Y;
-    }
+			if (canMoveRight) x++;
+		}
+		
+		if (wind[windIndex] == '<')
+		{
+			var canMoveLeft = true;
+			RockLoop(rock, x, y, ((caveX, caveY) =>
+			{
+				if (caveX - 1 < 0 || cave[caveY, caveX - 1] == '#') canMoveLeft = false;
+			}));
+
+			if (canMoveLeft) x--;
+		}
+		
+		//Console.WriteLine($"After moved by wind {wind[windIndex]}");	
+		//DrawCave(0, 10, rock, x, y);
+		
+		// Fall
+		RockLoop(rock, x, y, ((caveX, caveY) =>
+		{
+			if (caveY - 1 < 0 || cave[caveY - 1, caveX] == '#')
+			{
+				landed = true;
+			}
+		}));
+
+		if (!landed) y--;
+		windIndex++;
+		if (windIndex >= wind.Length) windIndex = 0;
+		
+		//Console.WriteLine("Fall");	
+		//DrawCave(0, 10, rock, x, y);
+	}
+
+	// Add rock
+	RockLoop(rock, x, y, (caveX, caveY) => { cave[caveY, caveX] = '#'; });
+
+	if (y + 3 + rockHeights[i % 5] > topY) topY = y + 3 + rockHeights[i % 5];
 }
 
-    drawGrid(450, 0, 150, 50);
+Console.WriteLine($"Part1: {topY - 3}");
 
-var sand = new List<SPos>();
-var part1 = 0;
-//for (int step = 0; step < 10; step++)
-var step = 0;
-while (true)
+void DrawCave(int y, int length, char[,]? rock = null, int rx = -10, int ry = 0)
 {
-    for (int i = 0; i < sand.Count; i++)
-    {
-        var s = sand[i];
-        if (s.Done) continue;
-        
-        if (!(grid[s.Y + 1, s.X] == '#' || grid[s.Y + 1, s.X] == 'o'))
-        {
-            s.Y++;
-            grid[s.Y, s.X] = '~';
-        } else if (!(grid[s.Y + 1, s.X - 1] == '#' || grid[s.Y + 1, s.X - 1] == 'o'))
-        {
-            s.Y++;
-            s.X--;
-            grid[s.Y, s.X] = '~';
-        } else if (!(grid[s.Y + 1, s.X + 1] == '#' || grid[s.Y + 1, s.X + 1] == 'o'))
-        {
-            s.Y++;
-            s.X++;
-            grid[s.Y, s.X] = '~';
-        }
-        else
-        {
-            grid[s.Y, s.X] = 'o';
-            part1++;
-            s.Done = true;
-        }
+	Console.WriteLine("Cave");
+	for (int caveY = y + length; caveY >= y; caveY--)
+	{
+		for (int caveX = 0; caveX < 7; caveX++)
+		{
+			if (caveX >= rx && caveX < rx + 4 && caveY >= ry && caveY < ry + 4 && rock != null && rock[caveY - ry, caveX - rx] == '@')
+			{
+					Console.Write('@');
+			}
+			else
+			{
+				Console.Write(cave[caveY, caveX]);
+			}
+		}
 
-        sand[i] = s;
-    }
+		Console.WriteLine();
+	}
 
-    if (grid[0, 500] == 'o')
-    {
-        break;
-    }
-    sand.Add(new SPos(500, 0));
-    //drawGrid(450, 0, 100, 20);
-    //Thread.Sleep(100);
-    if (step % 500 == 0)
-    {
-        Console.WriteLine($"Part1: {part1}");
-    }
-
-    step++;
-}
-
-Console.WriteLine($"Part1: {part1}");
-//drawGrid(0, 0, 1000, 1000);
-    drawGrid(450, 0, 150, 20);
-
-record Pos(int X, int Y);
-struct SPos
-{
-    public int X;
-    public int Y;
-    public bool Done = false;
-
-    public SPos(int x, int y)
-    {
-        X = x;
-        Y = y;
-    }
+	Console.WriteLine();
 }
---
+++ AoC22/day14.cs
@@ -0,0 +1,175 @@
+namespace AoC22;
+
+using System.Text.RegularExpressions;
+
+public class Day14
+{
+	public static void Solve()
+	{
+		var rx = new Regex(@"(\d+),(\d+)");
+		var lines = File.ReadLines("../../../day14.txt")
+			.Select(line =>
+			{
+				var matches = rx.Match(line);
+				var coords = new List<Pos>();
+				foreach (Match match in rx.Matches(line))
+				{
+					var split = match.Value.Split(",");
+					coords.Add(new Pos(int.Parse(split[0]), int.Parse(split[1])));
+				}
+
+				return coords;
+			})
+			.ToList();
+
+		var grid = new char[1000, 1000];
+		for (var y = 0; y < grid.GetLength(0); y++)
+		{
+			for (var x = 0; x < grid.GetLength(1); x++)
+			{
+				grid[y, x] = '.';
+			}
+		}
+
+		void drawGrid(int x, int y, int w, int h)
+		{
+			for (int gy = y; gy < y + h; gy++)
+			{
+				for (int gx = x; gx < x + w; gx++)
+				{
+					Console.Write(grid![gy, gx]);
+				}
+
+				Console.WriteLine();
+			}
+
+			Console.WriteLine();
+		}
+
+		var diff = 0;
+		foreach (var lineChain in lines)
+		{
+			var x = lineChain[0].X;
+			var y = lineChain[0].Y;
+			for (int i = 1; i < lineChain.Count; i++)
+			{
+				var dx = lineChain[i].X - x;
+				var dy = lineChain[i].Y - y;
+				if (dx < 0) x = lineChain[i].X;
+				if (dy < 0) y = lineChain[i].Y;
+
+				for (int lx = x; lx <= x + Math.Abs(dx); lx++)
+				{
+					for (int ly = y; ly <= y + Math.Abs(dy); ly++)
+					{
+						grid[ly, lx] = '#';
+					}
+				}
+
+				x = lineChain[i].X;
+				y = lineChain[i].Y;
+			}
+		}
+
+		var lineY = 1000;
+		for (int gy = 999; gy > 0; gy--)
+		{
+			var hasWall = false;
+			for (int gx = 0; gx < 1000; gx++)
+			{
+				if (grid[gy, gx] == '#')
+				{
+					hasWall = true;
+				}
+			}
+
+			if (hasWall)
+			{
+				lineY = gy;
+				break;
+			}
+		}
+
+		Console.WriteLine($"max Y is {lineY}");
+
+		for (int x = 0; x < 1000; x++)
+		{
+			grid[lineY + 2, x] = '#';
+		}
+
+		drawGrid(450, 0, 150, 50);
+
+		var sand = new List<SPos>();
+		var part1 = 0;
+		//for (int step = 0; step < 10; step++)
+		var step = 0;
+		while (true)
+		{
+			for (int i = 0; i < sand.Count; i++)
+			{
+				var s = sand[i];
+				if (s.Done) continue;
+
+				if (!(grid[s.Y + 1, s.X] == '#' || grid[s.Y + 1, s.X] == 'o'))
+				{
+					s.Y++;
+					grid[s.Y, s.X] = '~';
+				}
+				else if (!(grid[s.Y + 1, s.X - 1] == '#' || grid[s.Y + 1, s.X - 1] == 'o'))
+				{
+					s.Y++;
+					s.X--;
+					grid[s.Y, s.X] = '~';
+				}
+				else if (!(grid[s.Y + 1, s.X + 1] == '#' || grid[s.Y + 1, s.X + 1] == 'o'))
+				{
+					s.Y++;
+					s.X++;
+					grid[s.Y, s.X] = '~';
+				}
+				else
+				{
+					grid[s.Y, s.X] = 'o';
+					part1++;
+					s.Done = true;
+				}
+
+				sand[i] = s;
+			}
+
+			if (grid[0, 500] == 'o')
+			{
+				break;
+			}
+
+			sand.Add(new SPos(500, 0));
+			//drawGrid(450, 0, 100, 20);
+			//Thread.Sleep(100);
+			if (step % 500 == 0)
+			{
+				Console.WriteLine($"Part1: {part1}");
+			}
+
+			step++;
+		}
+
+		Console.WriteLine($"Part1: {part1}");
+		//drawGrid(0, 0, 1000, 1000);
+		drawGrid(450, 0, 150, 20);
+	}
+
+	record Pos(int X, int Y);
+
+	struct SPos
+	{
+		public int X;
+		public int Y;
+		public bool Done = false;
+
+		public SPos(int x, int y)
+		{
+			X = x;
+			Y = y;
+		}
+	}
+}
